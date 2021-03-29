1c5d68678f224b6ce94ef212bb39300e4c5a46e8
Mars <marswiz@yeah.net>
2021-03-30 02:24:36 +0800

    add

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
-read -p '这次提交的描述信息是？' commitInfo
+read -p '这次提交的描述信息是？' -a commitInfo
 while [ -z $commitInfo ]
     do
-    read -p '提交信息不能为空, 请重新输入:' commitInfo
+    read -p '提交信息不能为空, 请重新输入:' -a commitInfo
 done
 
 git add .
