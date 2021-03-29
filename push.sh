ef241d00607b5738826ba3c8451b2d55cd93129f
Mars <marswiz@yeah.net>
2021-03-30 02:19:05 +0800

    commitInfo

--- push.sh
+++ push.sh
@@ -1,3 +1,10 @@
+read -p '这次提交的描述信息是？' commitInfo
+while [ -z $commitInfo ]
+    do
+    read -p '提交信息不能为空, 请重新输入:' commitInfo
+done
+
 git add .
-git commit -m 'updated'
+git commit -m commitInfo
 git push origin master
+echo 'Completely Finished.'
